f36e8eb7b4fc356388cb0620de9a9768c64b2ed3
Lucas Stofaleti <lucastofaleti@hotmail.com>
2021-01-15 17:02:54 -0300

    Classe Department pronta, Enum WorkerLevel pronto.

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import entities.Department;
+import entities.enums.WorkerLevel;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Enter department's name: ");
+		Department dpte = new Department(sc.next());
+		
+		System.out.println("Enter worker data:");
+		System.out.print("Name: ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		
+		System.out.print("Level: ");
+		WorkerLevel level = WorkerLevel.valueOf(sc.next());
+		
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		
+		sc.close();
+	}
+}
